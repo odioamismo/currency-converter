a4b31ab736e02bf76ba7516e03b4a1b6d7707642
odioamismo <trojan.kacper00@gmail.com>
2023-07-27 11:28:21 +0200

    DOM nodes

---
+++ app.js
@@ -0,0 +1,7 @@
+const fromCurrency = document.querySelector(".from-currency select");
+const toCurrency = document.querySelector(".to-currency select");
+const fromAmount = document.querySelector(".from-amount input");
+const fromResult = document.getElementById("from-result");
+const toResult = document.getElementById("to-result");
+const convertButton = document.getElementById("convert-btn");
+const swapButton = document.getElementById("swap-btn");
