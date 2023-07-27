855b0c41d2ea80eeb4d3e3e49468ce6ab3d2bff3
odioamismo <trojan.kacper00@gmail.com>
2023-07-27 12:34:00 +0200

    Added async function to fetch data from API

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const fromResult = document.getElementById("from-result");
 const toResult = document.getElementById("to-result");
 const convertButton = document.getElementById("convert-btn");
 const swapButton = document.getElementById("swap-btn");
+
+const loadCountrySymbols = async () => {
+    const API_URL = `https://api.exchangerate.host/symbols`;
+    const result = await fetch(API_URL);
+    const data = await result.json()
+    let listOfSymbols = data.symbols;
+}
+
+loadCountrySymbols(); 
